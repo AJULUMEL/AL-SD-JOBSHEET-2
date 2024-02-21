13786af4c019d1b883fd35c47f9c99a0e4e5443b
AJULUMEL <azruljoss@gmail.com>
2024-02-21 20:59:56 +0700

    buku06

---
+++ src/Buku06.java
@@ -0,0 +1,30 @@
+public class Buku06 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0 && jml <= stok) {
+            stok -= jml;
+            System.out.println(jml + " buku terjual.");
+        } else {
+            System.out.println("Stok tidak mencukupi untuk melakukan penjualan.");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
